a05e2f3f3523fd49e742a5290c785aa8c829b24d
alex <a.voitekhovich@gmail.com>
2021-08-31 12:26:40 +0300

    fix

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
